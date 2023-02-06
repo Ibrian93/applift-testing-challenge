52d67e5a071068afdd745f863b0e52a9ce1beba5
Brian Icochea Lopez <ibrian93@gmail.com>
2023-02-06 20:58:52 +0100

    Finishing setup for E2E testing

---
+++ cypress/support/commands.js
@@ -0,0 +1,12 @@
+import '.commands'
+
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+}
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.get('').type(username)
+    cy.get('').type(password)
+    cy.get().click()
+}
